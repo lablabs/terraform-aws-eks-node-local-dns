2342cb75183ca82a775b76f2258aa30cd375a076
Michal Muransky <michal.muransky@lablabs.io>
2023-06-23 16:36:39 +0200

    feat: initial

--- values.tf
+++ values.tf
@@ -1,6 +1,140 @@
 locals {
   values_default = yamlencode({
-    # add default values here
+        "config": {
+        "localDnsIp": "169.254.20.11",
+        "zones": [
+            {
+                "zone": ".:53",
+                "plugins": {
+                    "errors": true,
+                    "reload": true,
+                    "debug": false,
+                    "log": {
+                        "format": "combined",
+                        "classes": "all"
+                    },
+                    "cache": {
+                        "parameters": 30,
+                        "denial": {},
+                            # size: 0
+                            # ttl: 1
+                        "success": {},
+                            # size: 8192
+                            # ttl: 30
+                        "prefetch": {},
+                            # amount: 1
+                            # duration: 10m
+                            # percentage: 20%
+                        "serve_stale": false
+                    },
+                    "forward": {
+                        "parameters": "172.20.0.10",
+                        "force_tcp": false,
+                        "prefer_udp": false,
+                        "policy": "",  # random|round_robin|sequential
+                        "max_fails": "",
+                        "expire": "",
+                        "health_check": "",
+                        "except": ""
+                    },
+                    "prometheus": true,
+                    "health": {
+                        "port": 8080
+                    }
+                }
+            },
+            {
+                "zone": "ip6.arpa:53",
+                "plugins": {
+                    "errors": true,
+                    "reload": true,
+                    "debug": false,
+                    "log": {
+                        "format": "combined",
+                        "classes": "all"
+                    },
+                    "cache": {
+                        "parameters": 30
+                    },
+                    "forward": {
+                        "parameters": "172.20.0.10",
+                        "force_tcp": false
+                    },
+                    "prometheus": true,
+                    "health": {
+                        "port": 8080
+                    }
+                }
+            },
+            {
+                "zone": "in-addr.arpa:53",
+                "plugins": {
+                    "errors": true,
+                    "reload": true,
+                    "debug": false,
+                    "log": {
+                        "format": "combined",
+                        "classes": "all"
+                    },
+                    "cache": {
+                        "parameters": 30
+                    },
+                    "forward": {
+                        "parameters": "172.20.0.10",
+                        "force_tcp": false
+                    },
+                    "prometheus": true,
+                    "health": {
+                        "port": 8080
+                    }
+                }
+            }
+        ]
+    },
+    "useHostNetwork": true,
+    "updateStrategy": {
+        "rollingUpdate": {
+            "maxUnavailable": "10%"
+        }
+    },
+    "priorityClassName": "system-node-critical",
+    "podAnnotations": {},
+    "podSecurityContext": {},
+    "securityContext": {
+        "privileged": true
+    },
+    "readinessProbe": null,
+    "serviceAccount": {
+        "create": true,
+        "annotations": {},
+        "name": ""
+    },
+    "nodeSelector": {},
+    "affinity": {},
+    "tolerations": [
+        {
+            "key": "CriticalAddonsOnly",
+            "operator": "Exists"
+        },
+        {
+            "effect": "NoExecute",
+            "operator": "Exists"
+        },
+        {
+            "effect": "NoSchedule",
+            "operator": "Exists"
+        }
+    ],
+    "resources": {
+        "requests": {
+            "cpu": "30m",
+            "memory": "50Mi"
+        }
+    },
+    "metrics": {
+        "prometheusScrape": "true",
+        "port": 9253
+    }
   })
 }
 
